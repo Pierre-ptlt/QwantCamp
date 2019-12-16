9d6df2eb7e27a1dc74f9a7b90412537c5c93e133
galbit_t <galbit_t@etna-alternance.net>
2019-12-16 15:14:45 +0100

    test

--- qwant/src/index.js
+++ qwant/src/index.js
@@ -3,10 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import APIconfig from './APIconfig.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//ReactDOM.render(<Menu />, document.getElementById('body'));
+ReactDOM.render(<APIconfig />, document.getElementById('body'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
